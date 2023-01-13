8cfb1fda5458917ebfea5f0bac5601ba63837e11
Andres <andreciito38@gmail.com>
2023-01-13 10:31:02 -0500

    Intento 1

--- src/CalendarApp.jsx
+++ src/CalendarApp.jsx
@@ -1,5 +1,5 @@
 import { Provider } from "react-redux"
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter, HashRouter } from "react-router-dom"
 import { AppRouter } from "./router"
 import { store } from "./store"
 
@@ -7,9 +7,11 @@ export const CalendarApp = () => {
     return (
         <>
             <Provider store={ store }>
-                <BrowserRouter>
+                {/* <BrowserRouter> */}
+                <HashRouter>
                     <AppRouter/>    
-                </BrowserRouter>
+                </HashRouter>
+                {/* </BrowserRouter> */}
             </Provider>
         </>
     )
